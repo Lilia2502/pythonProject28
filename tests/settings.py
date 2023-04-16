bb37677467c6014fc82bfa00b943e1350d056d7e
Lilia2502 <lilichka_250286@mail.ru>
2023-04-17 01:31:30 +0300

    Initial commit

---
+++ tests/settings.py
@@ -0,0 +1,25 @@
+class Settings:
+    chrome_driver = "/tests/chromedriver.exe"
+
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'lilichka_250286@mail.ru'
+    valid_password = 'L6rCzG8JXjd_eba'
+    valid_email_reg = 'Ввести валидный email'
+    invalid_email = 'invalide@mail.ru'
+    invalid_password = 'inv123'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Лилия'
+    last_name = 'Валиуллина'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'р'
+    latin_generate_string = 'incorrect'
+    special_chars = '()_+=/{}!@#$%^&*'
+    numbers = 777
+    empty = ''
+    passw1 = 'Ghhyud'
+    passw2 = 'gjdjmd'
+    passw3 = 'hsjkud'
